80b62c232110c51587ef7b78ccb6652766316d11
myasnik131995 <m.gorlenkov@yahoo.com>
2022-02-10 08:53:00 +0300

    pushAddFilmsInArray

--- randomGenerator/randomScript.js
+++ randomGenerator/randomScript.js
@@ -1,9 +1,4 @@
-let changeArray = [
-    'Фильм №1 от Насти',
-    'Фильм №1 от Максима',
-    'Фильм №2 от Насти',
-    'Фильм №2 от Максима',
-];
+let changeArray = [];
 let btnChange = document.querySelector('#button_change');
 let addText = document.querySelector('#change');
 
@@ -18,3 +13,23 @@ let btnWatch = document.querySelector('#btn_watch');
 btnWatch.onclick = function () {
     document.location.href = "/watch/watch.html";
 };
+
+
+
+// функционал для добавления фильмов в массив
+let btnAddFilms = document.querySelector('#btn_add');
+let inputFilm = document.querySelector('#input_film');
+
+function addFilmsOnClick() {
+    changeArray.push(inputFilm.value);
+    console.log(changeArray);
+    inputFilm.value = '';
+
+}
+
+btnAddFilms.addEventListener('click', addFilmsOnClick);
+inputFilm.addEventListener('keyup', function(e){
+    if(e.keyCode === 13){
+        addFilmsOnClick();
+    }
+});
